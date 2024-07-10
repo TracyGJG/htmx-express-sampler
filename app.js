3eb8e08aaa3978eeea70131f801d9ff02ab80301
TGJ Gilmore <TracyGJG@users.noreply.github.com>
2024-07-10 16:37:16 +0100

    Reorganised

--- app.js
+++ app.js
@@ -4,14 +4,14 @@ const bodyParser = require('body-parser');
 
 const postRoutes = require('./routes/post');
 const getRoutes = require('./routes/get');
-const PageRoute = require('./routes/pageRouter.js');
+const PageRoute = require('./views/screens.js');
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.set(express.static(path.join(__dirname, 'views')));
+app.set(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (_req, res) => {
   res.sendFile(path.join(__dirname, 'views/index.html'));
