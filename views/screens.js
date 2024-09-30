38c3cf378690b65ea56d7aa400563909850c6216
TGJ Gilmore <TracyGJG@users.noreply.github.com>
2024-09-30 16:54:42 +0100

    Minor correction

--- views/screens.js
+++ views/screens.js
@@ -1,6 +1,7 @@
+const path = require('path');
 const express = require('express');
+
 const PageRouter = express.Router();
-var path = require('path');
 
 PageRouter.use('/post', (req, res) => {
   res.sendFile(path.join(__dirname, '../Static/Post/post.html'));
